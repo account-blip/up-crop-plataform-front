7b4ae6f730cf2737bb7f4755dbcd504d3184bc93
diegoreyerosadaptia <diego@adaptianow.com>
2025-10-22 12:27:10 -0300

    fix: build

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import { AuthForm } from "../components/auth-form";
-
+export const dynamic = "force-dynamic";
 export default function LoginPage() {
   return (
     <Suspense fallback={<div>Cargando...</div>}>
